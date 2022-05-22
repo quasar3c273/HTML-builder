f99da018aee0768b0622c14d758228041cba9b4d
Mikhail_Lapitskiy <mikhail.lapitskiy@nordclan>
2022-05-23 00:43:48 +0300

    feat: add css for 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,9 +1,23 @@
 const fsp = require('fs/promises');
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 const copiedFolder = path.join(__dirname, 'project-dist');
 
-
+// const mainFolder = path.join(__dirname, 'styles');
+//
+// async function copyDir(source, target) {
+//   await fsp.rm(target, { recursive: true, force: true });
+//   await fsp.mkdir(target);
+//
+//   const files = await fsp.readdir(source, { withFileTypes: true });
+//   files.forEach((file) => {
+//     if (!file.isFile()) {
+//       copyDir(path.join(source, file.name), path.join(target, file.name));
+//     } else {
+//       fsp.copyFile(path.join(source, file.name), path.join(target, file.name));
+//     }
+//   });
+// }
 
 async function buildPage() {
   await fsp.rm(copiedFolder, { recursive: true, force: true });
@@ -21,6 +35,22 @@ async function buildPage() {
     let newIndexTemplate = indexTemplate.replace(tag, fileContent);
     await fsp.writeFile(path.join(copiedFolder, 'index.html'), newIndexTemplate);
   }
+
+  const folderStyles = path.join(__dirname, 'styles');
+  // внутри папки project-dist создать пустой файл style.css
+
+  const endFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+  fs.readdir(folderStyles, { withFileTypes: true }, (err, files) => {
+    files.forEach((file) => {
+      if (path.extname(file.name) === '.css') {
+        const readableStream = fs.createReadStream(path.join(folderStyles, file.name));
+        readableStream.on('data', chunk => endFile.write(chunk));
+      }
+    });
+  });
+
+  // Копирует папку assets в project-dist/assets
+  // await copyDir(mainFolder, copiedFolder);
 }
 
 buildPage();
